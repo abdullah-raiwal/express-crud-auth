0c7c0058b69ca5677ea41b0dc35f491f3422d9b7
abdullah-raiwal <abdullahh.qaiser@gmail.com>
2023-08-06 22:12:42 +0500

    added user jwt for sign-up and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,70 @@
 const prisma = require('../lib/db')
 const { userSchema } = require('../schema-validator/userSchema')
+const bcrypt = require('bcryptjs')
+const jsonwebtoken = require('jsonwebtoken')
 
 exports.signUp = async (req, res) => {
 
     try {
         // fetch user data from request body
         const userData = req.body
-        console.log("🚀 ~ file: authController.js:9 ~ exports.signUp= ~ userData:", userData)
 
         // validate user data using userSchema build with yup
-        const userCheck = await userSchema.validate(userData)
-        console.log("🚀 ~ file: authController.js:13 ~ exports.signUp= ~ userCheck:", userCheck)
+        await userSchema.validate(userData)
+        delete userData.confirmPassword
+        // hash password using bcrypt
+        const hashedPassword = await bcrypt.hash(userData.password, 10);
+
         // if user data is valid, create a new user in the database
         const user = await prisma.user.create({
-            data: userData
+            data: {
+                ...userData,
+                password: hashedPassword
+            }
         })
-        console.log("🚀 ~ file: authController.js:17 ~ exports.signUp= ~ user:", user)
-        res.status(201).json({ message: "User created successfully", user: user })
+        // delete user data from request body
+        delete user.password
+        const jwtToken = jsonwebtoken.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: 300
+        })
+
+        res.status(201).json({ message: "User created successfully", result: { user, jwtToken } })
 
     } catch (error) {
         console.log("🚀 ~ file: authController.js:22 ~ exports.signUp= ~ error:", error)
         res.status(400).json({ error: error.message })
+    }
+
+}
+
+exports.login = async (req, res) => {
+
+    try {
+        const { username, password } = req.body
+        // await userSchema.validate({ username, password })
+
+        const user = await prisma.user.findUnique({
+            where: {
+                username
+            }
+        })
+        if (!user) {
+            return res.status(401).json({ message: "user not exists" })
+        }
+        const validPassword = await bcrypt.compare(password, user.password)
+        if (!validPassword) {
+            return res.status(401).json({ message: "Invalid username or password" })
+        }
+        const jwtToken = jsonwebtoken.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: 300
+        })
+        res.status(200).json({ message: "User logged in successfully", result: { id: user._id, jwt: jwtToken } })
+
+
+    } catch (error) {
+        console.log("🚀 ~ file: authController.js:65 ~ exports.login= ~ error:", error)
+        res.status(400).json({ error: error.message })
+
     }
 
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { createUser, deleteUser, getAllUsers, getUser, updateUser } = require('../controllers/userController')
-const {signUp} = require('../controllers/authController')
+const { signUp, login } = require('../controllers/authController')
 
 const userRouter = express.Router();
 // method to sign up a new user
 userRouter.route('/signup').post(signUp);
+userRouter.route('/login').post(login);
+
 // methods to /api/user
 userRouter.route('/').get(getAllUsers).post(createUser)
 userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
